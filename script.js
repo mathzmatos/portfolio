177e6d496c1b4caf488cbfab464d99568620550c
Matheus Matos <cyrekt@hotmail.com>
2022-05-18 16:42:22 -0300

    changes

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var typed = new Typed(".typing", {
-    strings: ["Olá", "Hello", "Salut", "Hola"],
+    strings: ["Olá", "Hello", "Hola"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+var typed = new Typed(".typing-2", {
+    strings: ["C#", "C++", "HTML", "CSS", "JavaScript", "Python", "R"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
