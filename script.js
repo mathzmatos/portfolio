0e62b14dabae67ec6021e82dfa08c90771b90f71
mathzmatos <cyrekt@hotmail.com>
2023-02-28 19:29:58 -0300

    update

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ var typed = new Typed(".typing-3", {
     loop: true
     
 });
+
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  var boxes = document.querySelectorAll('.box');
+  
+  function checkVisibility() {
+    for (var i = 0; i < boxes.length; i++) {
+      var box = boxes[i];
+      
+      if (isElementInViewport(box)) {
+        box.classList.add('visible');
+      } else {
+        box.classList.remove('visible');
+      }
+    }
+  }
+  
+  window.addEventListener('load', checkVisibility);
+  window.addEventListener('scroll', checkVisibility);
